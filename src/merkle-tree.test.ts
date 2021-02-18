0a7ba830fca388adbf33a9dfd79d93672b7e0479
Amir Yahalom <amir.ylm4@gmail.com>
2021-02-18 19:35:39 +0200

    fixes and tests

--- src/merkle-tree.test.ts
+++ src/merkle-tree.test.ts
@@ -5,59 +5,82 @@ import enc from 'crypto-js/enc-base64';
 describe('merkle-tree', () => {
     describe('hash', () => {
         it('should create a merkle root', () => {
-            const mt = new MerkleTree(hash, enc, ["aaa", "bbb", "ccc"]);
+            const mt = new MerkleTree(hash, enc, ['aaa', 'bbb', 'ccc']);
             const root = mt.hash();
-            expect(root).toEqual('ScIuiTF4x8dp9wAGhKzmqVeNQHqXV/Gy4SaCszW/YPOGBJ3tfmfVBbG0bk16OfS9aPxNLk2s5V4lr5/+aFfAWg==');
+            expect(root).toEqual(
+                'ScIuiTF4x8dp9wAGhKzmqVeNQHqXV/Gy4SaCszW/YPOGBJ3tfmfVBbG0bk16OfS9aPxNLk2s5V4lr5/+aFfAWg==',
+            );
         });
     });
 
     describe('height', () => {
         it('should return currect height merkle root', () => {
-            const mt = new MerkleTree(hash, enc, ["aaa", "bbb", "ccc"]);
+            const mt = new MerkleTree(hash, enc, ['aaa', 'bbb', 'ccc']);
+            expect(mt.height()).toEqual(2);
+        });
+    });
+
+    describe('addLeaves', () => {
+        it('should add the given leaves to the tree', () => {
+            const mt = new MerkleTree(hash, enc);
+            mt.addLeaves('aaa', 'bbb', 'ccc');
             expect(mt.height()).toEqual(2);
         });
     });
 
     describe('verify', () => {
         it('should verify the proof', () => {
-            const mt = new MerkleTree(hash, enc, ["aaa", "bbb", "ccc"]);
-            mt.hash(console.log);
+            const mt = new MerkleTree(hash, enc, ['aaa', 'bbb', 'ccc']);
+            mt.hash();
             const proof: Proof = {
                 path: [
-                    { dir: 0, data: 'rEXF4ALsag+SQM0qk276UoJarrOL0e+EmWN9A4dwJz/te0NSG8PslhVQSLoGn8WK9LB7TMUf10iAfG7EVnq64w==' },
-                    { dir: 1, data: 'wHzvzcqOjAxjXJ2JkCAiTTKjqzqyHNVxp8WvRFr+6hk1uzzzBa8mJd1P7vWtYy1Kf7SakUDeNdIIThH4jjVdKg==' }
+                    {
+                        dir: 0,
+                        data:
+                            'rEXF4ALsag+SQM0qk276UoJarrOL0e+EmWN9A4dwJz/te0NSG8PslhVQSLoGn8WK9LB7TMUf10iAfG7EVnq64w==',
+                    },
+                    {
+                        dir: 1,
+                        data:
+                            'wHzvzcqOjAxjXJ2JkCAiTTKjqzqyHNVxp8WvRFr+6hk1uzzzBa8mJd1P7vWtYy1Kf7SakUDeNdIIThH4jjVdKg==',
+                    },
                 ],
-                data: 'X+5VKciDopF2eZsOzhi6fdj7xX5oStEfbsls2SM7MwEEkjFtb1mKpCQP5SFeuXZfiwVNz7Iutm8Mz3yUw07VLQ=='
+                data:
+                    'X+5VKciDopF2eZsOzhi6fdj7xX5oStEfbsls2SM7MwEEkjFtb1mKpCQP5SFeuXZfiwVNz7Iutm8Mz3yUw07VLQ==',
             };
             expect(mt.verify(proof)).toBeTruthy();
         });
 
         it('should verify non-valid proof', () => {
-            const mt = new MerkleTree(hash, enc, ["aaa", "bbb", "ccc"]);
-            mt.hash(console.log);
+            const mt = new MerkleTree(hash, enc, ['aaa', 'bbb', 'ccc']);
+            mt.hash();
             const proof: Proof = {
                 path: [
                     { dir: 0, data: 'xxx' },
-                    { dir: 1, data: 'wHzvzcqOjAxjXJ2JkCAiTTKjqzqyHNVxp8WvRFr+6hk1uzzzBa8mJd1P7vWtYy1Kf7SakUDeNdIIThH4jjVdKg==' }
+                    {
+                        dir: 1,
+                        data:
+                            'wHzvzcqOjAxjXJ2JkCAiTTKjqzqyHNVxp8WvRFr+6hk1uzzzBa8mJd1P7vWtYy1Kf7SakUDeNdIIThH4jjVdKg==',
+                    },
                 ],
-                data: 'X+5VKciDopF2eZsOzhi6fdj7xX5oStEfbsls2SM7MwEEkjFtb1mKpCQP5SFeuXZfiwVNz7Iutm8Mz3yUw07VLQ=='
+                data:
+                    'X+5VKciDopF2eZsOzhi6fdj7xX5oStEfbsls2SM7MwEEkjFtb1mKpCQP5SFeuXZfiwVNz7Iutm8Mz3yUw07VLQ==',
             };
             expect(mt.verify(proof)).toBeFalsy();
         });
     });
 
     describe('getProof', () => {
-        it('should create a proof', () => {
-            const mt = new MerkleTree(hash, enc, ["aaa", "bbb", "ccc"]);
+        it('should create a valid proof', () => {
+            const mt = new MerkleTree(hash, enc, [
+                'aaa',
+                'bbb',
+                'ccc',
+                'ddd',
+                'eee',
+                'fff',
+            ]);
             const proof = mt.getProof(1);
-            console.log(proof);
-            // const proof: Proof = {
-            //     path: [
-            //         { dir: 0, data: 'rEXF4ALsag+SQM0qk276UoJarrOL0e+EmWN9A4dwJz/te0NSG8PslhVQSLoGn8WK9LB7TMUf10iAfG7EVnq64w==' },
-            //         { dir: 1, data: 'wHzvzcqOjAxjXJ2JkCAiTTKjqzqyHNVxp8WvRFr+6hk1uzzzBa8mJd1P7vWtYy1Kf7SakUDeNdIIThH4jjVdKg==' }
-            //     ],
-            //     data: 'X+5VKciDopF2eZsOzhi6fdj7xX5oStEfbsls2SM7MwEEkjFtb1mKpCQP5SFeuXZfiwVNz7Iutm8Mz3yUw07VLQ=='
-            // };
             expect(mt.verify(proof)).toBeTruthy();
         });
     });
--- src/merkle-tree.ts
+++ src/merkle-tree.ts
@@ -30,11 +30,13 @@ export class MerkleTree {
     constructor(
         private readonly hasher: Hasher,
         private readonly encoder: Encoder,
-        private leaves: string[],
+        private leaves: string[] = [],
     ) {}
 
-    addLeaf(leaf: string): MerkleTree {
-        this.leaves.push(leaf);
+    addLeaves(...leaves: string[]): MerkleTree {
+        for (const leaf of leaves) {
+            this.leaves.push(leaf);
+        }
         return this;
     }
 
@@ -78,9 +80,12 @@ export class MerkleTree {
                         left.data + right.data,
                     );
                     const node = { data, left, right };
-                    !!iterator && iterator(node, h);
+                    !!iterator && iterator(node, h + 1);
                     level.push(node);
                 }
+            }
+            if (level.length % 2) {
+                level.push(level[level.length - 1]);
             }
             nodes = [...level];
         }
@@ -100,28 +105,35 @@ export class MerkleTree {
             }
             levels[height].push(current);
         });
+        const proofData = levels[0][i].data;
         const proof: Proof = {
             path: [],
-            data: levels[0][i].data,
+            data: proofData,
         };
         const h = this.height();
-        const data = hash(this.hasher, this.encoder, this.leaves[i]);
+        let data = proofData;
         for (let j = 0; j < h; j++) {
-            let i2 = i + 1;
+            let iNeighbor = i + 1;
             if (i % 2 !== 0) {
-                i2 = i - 1;
+                iNeighbor = i - 1;
             }
-            proof.path.push({ dir: i2 % 2 ? 1 : 0, data: levels[j][i2].data });
+            proof.path.push({
+                dir: i % 2 === 0 ? 1 : 0,
+                data: levels[j][iNeighbor].data,
+            });
             const level = levels[j + 1];
             if (!level) {
                 break;
             }
+            // decide what is the value of 'i' in the next level
             for (let iLevel = 0; iLevel < level.length; iLevel++) {
                 if (
                     level[iLevel].right?.data === data ||
                     level[iLevel].left?.data === data
                 ) {
                     i = iLevel;
+                    data = level[iLevel].data;
+                    break;
                 }
             }
         }
